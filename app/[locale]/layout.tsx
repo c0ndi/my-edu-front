0c7639c6d6f7cf2dac593633a9b07450ad43cd5d
c0ndi <kowalik0konrad@gmail.com>
2023-11-05 22:37:40 +0100

    fix

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme='light'
+          defaultTheme='dark'
           enableSystem={false}
           disableTransitionOnChange
         >
