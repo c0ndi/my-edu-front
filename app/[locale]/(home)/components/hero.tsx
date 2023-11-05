0dde261ea55211a1c7fb104df23229d7e7b87ee0
c0ndi <kowalik0konrad@gmail.com>
2023-11-05 23:29:13 +0100

    fix

--- app/[locale]/(home)/components/hero.tsx
+++ app/[locale]/(home)/components/hero.tsx
@@ -11,11 +11,11 @@ type Props = {}
 export default function Hero({ }: Props) {
   const t = useTranslations("hero")
   return (
-    <section className='max-w-[1280px] flex flex-col mx-auto py-12 px-2 overflow-hidden'>
+    <section className='max-w-[1280px] flex flex-col mx-auto py-16 px-2 overflow-hidden'>
       <div className='w-full flex items-center max-lg:flex-col'>
         <div className='w-1/2 max-lg:w-full'>
           <Badge text={t("badge")} />
-          <h1 className='font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-[#646464] to-[#0F172A] max-lg:text-3xl '>{t("heading")}</h1>
+          <h1 className='font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-[#646464] to-[#0F172A] max-lg:text-3xl '>{t("heading")}</h1>
           <p className='font-medium text-lg text-[#64748B] py-4 max-lg:text-base'>{t("description")}</p>
           <div className='flex gap-3 max-sm:flex-col'>
             <Link href={t("dashboardButton.link")}>
@@ -30,12 +30,11 @@ export default function Hero({ }: Props) {
             </Link>
           </div>
         </div>
-        <div className='w-1/2 translate-x-16 max-lg:w-full max-lg:translate-x-0'>
-          <Image
-            src={Preview}
-            alt={"Login app preview."}
-          />
-        </div>
+        <Image
+          src={Preview}
+          alt={"Login app preview."}
+          className='w-1/2 max-lg:w-full max-lg:translate-x-0'
+        />
       </div>
     </section>
 
