3f21f01fc4f5a2911dda70a815932d0a947683c4
c0ndi <kowalik0konrad@gmail.com>
2023-11-05 22:55:45 +0100

    fix

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,5 @@
 import { getRequestConfig } from "next-intl/server";
 
 export default getRequestConfig(async ({ locale }: { locale: string }) => ({
-  messages: (await import(`app/locales/${locale}/translation.json`)).default,
+  messages: await import(`app/locales/${locale}/translation.json`),
 }));
