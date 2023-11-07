ec93c3e63cee9d6926900f00a83b45a49b316287
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2023-11-07 23:52:06 +0100

    update

---
+++ app/[locale]/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function DashboardPage() {
+  return (
+    <div className="bg-[#1e1e1e]">
+        <div className="absolute z-[111] top-0 w-full h-screen loading"></div>
+        <iframe className="w-full h-screen" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F6E9whMDdNaYxDESlGnDExU%2FMYEDU-%252B-Quizker%3Ftype%3Ddesign%26node-id%3D0%253A1%26mode%3Ddesign%26t%3DgHk26DbTMXqrCLLq-1"></iframe>
+        <div className="absolute bottom-0 w-full h-16 bg-[#1e1e1e]"></div>
+    </div>
+  )
+}
